be33706c5ddc279fa9702e17596325de22ae66e3
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-25 19:26:50 +0530

    rectangle using numpy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2 # type: ignore
 from util import get_limits
 from PIL import Image # type: ignore
 
-blueColor = [255, 0, 0] # Blue color in BGR
+greenColor = [0, 255, 0] # Blue color in BGR
 
 # For capturing video from webcam
 cap = cv2.VideoCapture(0)
@@ -10,19 +10,29 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
+    if not ret:
+        break
+
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lowerLimit, upperLimit = get_limits(blueColor)
+    lowerLimit, upperLimit = get_limits(greenColor)
 
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
-    mask_ = Image.fromarray(mask)
+    # mask_ = Image.fromarray(mask)
 
-    bbox = mask_.getbbox()
+    # bbox = mask_.getbbox()
 
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    # if bbox is not None:
+    #     x1, y1, x2, y2 = bbox
+    #     frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    coords = cv2.findNonZero(mask)
+
+    if coords is not None:
+        x1, y1, w, h = cv2.boundingRect(coords)
+        x2, y2 = x1+w, y1+h 
+        frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (255, 0, 0), 5)
 
     cv2.imshow("Frame", frame)
 
