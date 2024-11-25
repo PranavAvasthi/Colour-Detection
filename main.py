4c76df700414532bfd53d2602ac66fb638e645c2
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-25 16:50:23 +0530

    Mask Created for detecting blue colour

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import cv2 # type: ignore
+from util import get_limits
+
+blueColor = [255, 0, 0] # Blue color in BGR
 
 # For capturing video from webcam
 cap = cv2.VideoCapture(0)
@@ -6,7 +9,13 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
-    cv2.imshow("Frame", frame)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(blueColor)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    cv2.imshow("Frame", mask)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
