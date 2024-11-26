512629d8df35218a0a4e1ace99ea2b27ea3a33fc
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-26 12:32:22 +0530

    Opening & Closing Morphology

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ while True:
     kernel = np.ones((5,5), np.uint8)
     erosion = cv2.erode(mask, kernel, iterations=1) #erosion
     dilation = cv2.dilate(erosion, kernel, iterations=1) #dilation
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
     contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
     
@@ -36,11 +36,13 @@ while True:
                 frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     # cv2.imshow("Frame", frame)
-    cv2.imshow("Mask", mask)
-    cv2.imshow("Result", res)
-    cv2.imshow("Erosion", erosion)
-    cv2.imshow("Dilation", dilation)
-    
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", res)
+    # cv2.imshow("Erosion", erosion)
+    # cv2.imshow("Dilation", dilation)
+    cv2.imshow("Opening", opening)
+    cv2.imshow("Closing", closing)
+
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
